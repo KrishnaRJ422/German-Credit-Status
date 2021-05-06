9a415df32a7614ddf54a6298ec4fa65967e855be
KrishnaRJ422 <62299325+KrishnaRJ422@users.noreply.github.com>
2021-05-07 02:06:49 +0530

    Update request.py

--- request.py
+++ request.py
@@ -5,11 +5,11 @@ url = 'http://localhost:5000/predict_score_api'
 
 request.get( {timeout: 1500})
 
-r = requests.post(url,json={'Gender':1,'Age':1,'Marital_Status':1,'NumMonths':24,'Savings_<500':1,'Savings_none':0,'Dependents':1,'Property_rent':0,
-                           'Job_management/self-emp/officer/highly qualif emp':0,'Debtors_guarantor':0,'Purpose_CarNew':0,
-                           'Purpose_furniture/equip':0,'CreditHistory_none/paid':0,'Purpose_CarUsed':0,'CreditAmount':0.04258831,
-                           'Collateral_real estate':1,'Debtors_none':1,'Job_unemp/unskilled-non resident':0,'Purpose_others':0,             
-                            'CreditHistory_other':0,'PayBackPercent':4,'Collateral_unknown/none':0,'Purpose_education':0
+r = requests.post(url,json={'Gender':1, 'Age':0, 'Marital_Status':1, 'CurrentAcc_None':0,
+       'CurrentAcc_LT200':0, 'Savings_LT500':1, 'CreditHistory_none/paid':1,
+       'Debtors_co-applicant':0, 'Job_unskilled-resident':0, 'NumMonths':36,
+       'Telephone':0, 'Purpose_education':0, 'Purpose_furniture/equip':0,
+       'CreditAmount':0.308, 'Foreignworker':1, 'Debtors_guarantor':0
 	
 })
 
