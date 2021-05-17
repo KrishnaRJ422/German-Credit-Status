e6186ab910d96d2962fc97bd3200a08e3894babf
KrishnaRJ422 <62299325+KrishnaRJ422@users.noreply.github.com>
2021-05-18 03:08:40 +0530

    Update request.py

--- request.py
+++ request.py
@@ -5,12 +5,11 @@ url = 'http://localhost:5000/predict_score_api'
 
 request.get( {timeout: 1500})
 
-r = requests.post(url,json={'Gender':1, 'Age':0, 'Marital_Status':1, 'CurrentAcc_None':0,
-       'CurrentAcc_LT200':0, 'Savings_LT500':1, 'CreditHistory_none/paid':1,
-       'Debtors_co-applicant':0, 'Job_unskilled-resident':0, 'NumMonths':36,
-       'Telephone':0, 'Purpose_education':0, 'Purpose_furniture/equip':0,
-       'CreditAmount':0.308, 'Foreignworker':1, 'Debtors_guarantor':0
-	
+r = requests.post(url,json={'CurrentAcc_None':0, 'NumMonths':36, 'CreditHistory_Delay':1,
+       'CreditHistory_none/paid':0, 'Collateral_savings/life_insurance':0,
+       'CurrentAcc_GE200':1, 'Purpose_repairs':0, 'Purpose_radio/tv':0, 'Gender':1,
+       'Age':1
+		
 })
 
 print(r.json())
